a451eb99e31dd21ce20a55d0208921ae5ea6e1e4
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2025-12-11 21:46:16 +0530

    Create HW_Leetcode_1934.sql

---
+++ Session 3/HW_Leetcode_1934.sql
@@ -0,0 +1,8 @@
+--G1_23BCS10799_TANMAY_SESSION_3
+-- Write your PostgreSQL query statement below
+select s.user_id, round(avg(case 
+when action = 'confirmed' then 1
+else 0 end),2) as confirmation_rate
+from signups s left join confirmations c
+on s.user_id = c.user_id
+group by s.user_id
